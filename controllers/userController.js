379a415c03294d228456097f54fc54b689491bfd
MohammedElKhadrawy <mohammedelkhadrawy641@gmail.com>
2023-09-12 18:50:43 +0300

    added getAllUsers and getSingleUser logic

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,19 +1,28 @@
+const User = require('../models/User');
+const throwCustomError = require('../errors/custom-error');
+
 exports.getAllUsers = async (req, res, next) => {
-  res.send('get all users');
-}
+  const users = await User.find({ role: 'user' }).select('-password');
+  res.status(200).json({ users });
+};
 
 exports.getSingleUser = async (req, res, next) => {
-  res.send('get single user');
-}
+  const { userId } = req.params;
+  const user = await User.findById(userId).select('-password');
+  if (!user) {
+    throwCustomError(`Could not find a user with ID: ${userId}`, 404);
+  }
+  res.status(200).json({ user });
+};
 
 exports.showCurrentUser = async (req, res, next) => {
   res.send('show current user');
-}
+};
 
 exports.updateUser = async (req, res, next) => {
   res.send('update user');
-}
+};
 
 exports.updateUserPassword = async (req, res, next) => {
   res.send('update user password');
-}
+};
