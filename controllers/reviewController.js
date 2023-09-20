e4df942a745630bb3493d9f90f0c3b4df4c996e3
MohammedElKhadrawy <mohammedelkhadrawy641@gmail.com>
2023-09-20 23:59:15 +0300

    added validation, CRUD logic for reviewController

--- controllers/reviewController.js
+++ controllers/reviewController.js
@@ -1,19 +1,67 @@
+const Product = require('../models/Product');
+const Review = require('../models/Review');
+const throwCustomError = require('../errors/custom-error');
+const { collectValidationResult } = require('../utils');
+
 exports.createReview = async (req, res, next) => {
-  res.send('create review');
+  collectValidationResult(req);
+  const { product: productId } = req.body;
+  const isValidProduct = await Product.findById(productId);
+  if (!isValidProduct) {
+    throwCustomError(`Could not find a product with ID: ${productId}`, 404);
+  }
+  const reviewData = { ...req.body, user: req.user.userId };
+  const review = await Review.create(reviewData);
+  res.status(201).json({ review });
 };
 
 exports.getAllReviews = async (req, res, next) => {
-  res.send('get all reviews');
+  const reviews = await Review.find().populate('product', 'name company price');
+  res.status(200).json({ reviews, count: reviews.length });
 };
 
 exports.getSingleReview = async (req, res, next) => {
-  res.send('get single review');
+  const { reviewId } = req.params;
+  const review = await Review.findById(reviewId);
+  if (!review) {
+    throwCustomError(`Could not find a review with ID: ${reviewId}`, 404);
+  }
+  res.status(200).json({ review });
 };
 
 exports.updateReview = async (req, res, next) => {
-  res.send('update review');
+  collectValidationResult(req);
+  const { reviewId } = req.params;
+  const review = await Review.findById(reviewId);
+  if (!review) {
+    throwCustomError(`Could not find a review with ID: ${reviewId}`, 404);
+  }
+  if (!review.user.equals(req.user.userId)) {
+    throwCustomError(
+      'Unauthorized, only the user who created this review can update it',
+      403
+    );
+  }
+  for (const prop in req.body) {
+    if (prop === 'user' || prop === 'product') continue;
+    review[prop] = req.body[prop];
+  }
+  await review.save();
+  res.status(200).json({ review });
 };
 
 exports.deleteReview = async (req, res, next) => {
-  res.send('delete review');
+  const { reviewId } = req.params;
+  const review = await Review.findById(reviewId);
+  if (!review) {
+    throwCustomError(`Could not find a review with ID: ${reviewId}`, 404);
+  }
+  if (!review.user.equals(req.user.userId)) {
+    throwCustomError(
+      'Unauthorized, only the user who created this review can delete it',
+      403
+    );
+  }
+  await review.deleteOne();
+  res.status(200).json({ message: 'Successfully deleted review' });
 };
--- routes/reviewRoutes.js
+++ routes/reviewRoutes.js
@@ -1,19 +1,62 @@
 const express = require('express');
+const { body } = require('express-validator');
 
 const isAuth = require('../middleware/authentication');
 const reviewController = require('../controllers/reviewController');
+const Review = require('../models/Review');
 
 const router = express.Router();
 
 router
   .route('/')
   .get(reviewController.getAllReviews)
-  .post(isAuth, reviewController.createReview);
+  .post(
+    isAuth,
+    [
+      body('rating', 'rating must be a number between 1 and 5').isFloat({
+        min: 1,
+        max: 5,
+      }),
+      body('title', 'title must be a string less than 100 characters')
+        .isString()
+        .trim()
+        .isLength({ max: 100 }),
+      body('comment', 'comment is required').isString().notEmpty(),
+      body('product', 'product is required')
+        .notEmpty()
+        .custom(async (value, { req }) => {
+          const existingReview = await Review.findOne({
+            product: value,
+            user: req.user.userId,
+          });
+          if (existingReview) {
+            throw new Error('already submitted review for this product');
+          }
+        }),
+    ],
+    reviewController.createReview
+  );
 
 router
   .route('/:reviewId')
   .get(reviewController.getSingleReview)
-  .patch(isAuth, reviewController.updateReview)
+  .patch(
+    isAuth,
+    [
+      body('rating', 'rating must be a number between 1 and 5')
+        .optional()
+        .isFloat({
+          min: 1,
+          max: 5,
+        }),
+      body('title', 'title must be a string less than 100 characters')
+        .optional()
+        .isString()
+        .trim(),
+      body('comment', 'comment is required').optional().isString().notEmpty(),
+    ],
+    reviewController.updateReview
+  )
   .delete(isAuth, reviewController.deleteReview);
 
 module.exports = router;
